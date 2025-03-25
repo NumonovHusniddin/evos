e81fc384defe54ac6a3395e569f6d71d97af2cce
Husniddin <numonov.husnidin939@gmail.com>
2025-03-25 14:10:20 +0500

    commit

--- src/pages/about/Brends/BrendLocation.jsx
+++ src/pages/about/Brends/BrendLocation.jsx
@@ -15,11 +15,11 @@ function BrendLocation() {
     }
   return (
     <div className=" pt-[200px] container mb-[280px]">
-      <h1 className=" text-green-700 text-center text-[45px] font-bold mb-5">{findBrend?.title[i18n.language]}</h1>
-      <p className=" mb-3">{findBrend.desc1[i18n.language]}</p>
-      <p className=" mb-3">{findBrend.desc2[i18n.language]}</p>
-      <p className=" mb-3">{findBrend.desc3[i18n.language]}</p>
-      <p className=" mb-3">{findBrend.desc4[i18n.language]}</p>
+      <h1 className=" text-green-700 text-center text-[45px] font-bold mb-5 dark:text-white">{findBrend?.title[i18n.language]}</h1>
+      <p className=" mb-3 dark:text-white">{findBrend.desc1[i18n.language]}</p>
+      <p className=" mb-3 dark:text-white">{findBrend.desc2[i18n.language]}</p>
+      <p className=" mb-3 dark:text-white">{findBrend.desc3[i18n.language]}</p>
+      <p className=" mb-3 dark:text-white">{findBrend.desc4[i18n.language]}</p>
       <article className=" flex items-center justify-center">
         <button onClick={Back} className=" py-2 px-5 border border-green-700 rounded-lg text-green-700 hover:bg-green-700 hover:text-white transition-all duration-200">{t("ortga")}</button>
       </article>
